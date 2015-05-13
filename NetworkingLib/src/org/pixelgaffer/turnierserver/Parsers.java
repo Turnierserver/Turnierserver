ab7b1585041bb11751f763fb9ae6f31850c0a877
nicosio2 <nico.gruendel@web.de>
2015-05-13 20:49:18 +0200

    Finished Factory

--- NetworkingLib/src/org/pixelgaffer/turnierserver/Parsers.java
+++ NetworkingLib/src/org/pixelgaffer/turnierserver/Parsers.java
@@ -3,11 +3,18 @@ package org.pixelgaffer.turnierserver;
 public class Parsers {
 	
 	private static Parser defaultParser;
+	private static Parser compressingParser;
 	
-	public static Parser getParser(boolean encrypt) {
-		if(encrypt) {
-			return null;
+	public static Parser getParser(boolean compress) {
+		if(compress) {
+			if(compressingParser == null) {
+				compressingParser = new GsonGzipParser();
+			}
+			return compressingParser;
 		} else {
+			if(defaultParser == null) {
+				defaultParser = new GsonParser();
+			}
 			return defaultParser;
 		}
 	}
