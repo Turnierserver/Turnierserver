dc5c08e94a74125a9a899623c97dee35bc2ab253
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-06-02 18:16:05 +0200

    sandbox im tmux_start

--- tmux_start.sh
+++ tmux_start.sh
@@ -8,7 +8,8 @@ tmux new-session -d -s turnierserver
 tmux new-window -t turnierserver:1 -n 'Config' 'echo "turnierserver.prop:"; cat turnierserver.prop; echo "\n\nFrontend:"; python3 -m Frontend._cfg; read'
 tmux new-window -t turnierserver:2 -n 'Backend' './build/backend.sh "../turnierserver.prop"; read'
 tmux new-window -t turnierserver:3 -n 'Worker' './build/worker.sh "../turnierserver.prop"; read'
-tmux new-window -t turnierserver:4 -n 'Frontend' 'cd Frontend; python3 app.py run; read'
+tmux new-window -t turnierserver:4 -n 'Sandbox' 'sleep 1; java SandboxFake; read'
+tmux new-window -t turnierserver:5 -n 'Frontend' 'cd Frontend; python3 app.py run; read'
 
 tmux select-window -t turnierserver:1
 tmux -2 attach-session -t turnierserver
