3a8fc58f6c9e0372757a14b9a36c54e65df5c307
Dominic <meiserdo@web.de>
2015-06-02 14:56:38 +0200

    c++ zeugs im build script

--- buildNetworkZeugs.sh
+++ buildNetworkZeugs.sh
@@ -20,3 +20,18 @@ done
 echo -e "#!/bin/sh\ncd \`dirname \$0\`\njava -cp '*' org.pixelgaffer.turnierserver.backend.BackendMain \${@}" > build/backend.sh
 echo -e "#!/bin/sh\ncd \`dirname \$0\`\njava -cp '*' org.pixelgaffer.turnierserver.worker.WorkerMain \${@}" > build/worker.sh
 chmod +x build/*.sh
+
+cd build
+mkdir -p SandboxHelper
+cd SandboxHelper
+qmake -makefile ../../SandboxHelper/SandboxHelper.pro CONFIG+=debug
+make
+ln -s SandboxHelper/sandboxd_helper ../sandboxd_helper
+cd ..
+mkdir -p SandboxMachine
+cd SandboxMachine
+qmake -makefile ../../SandboxMachine/SandboxMachine.pro CONFIG+=debug
+make
+ln -s SandboxMachine/sandboxd ../sandboxd
+cd ..
+cd ..
