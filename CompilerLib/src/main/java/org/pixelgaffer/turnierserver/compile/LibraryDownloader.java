1ad48566d92dc3b2bb2488f2ba60d15c7ec15fd6
Dominic <meiserdo@web.de>
2015-06-03 10:15:47 +0200

    iface update für pythonische leute

--- CompilerLib/src/main/java/org/pixelgaffer/turnierserver/compile/LibraryDownloader.java
+++ CompilerLib/src/main/java/org/pixelgaffer/turnierserver/compile/LibraryDownloader.java
@@ -10,6 +10,18 @@ public interface LibraryDownloader
 	public File[] getAiLibs(String language);
 	
 	/**
+	 * Gibt die Datei mit dem angegebenen Namen der AiBibliothek zurück.
+	 */
+	public default File getAiLibFile (String language, String filename)
+	{
+		File[] files = getAiLibs(language);
+		for (File file : files)
+			if (file.getName().equals(filename))
+				return file;
+		return null;
+	}
+	
+	/**
 	 * Gibt alle Dateien zurück, die zu der Bibliothek mit dem angegebenen Namen gehören.
 	 */
 	public File[] getLib (String language, String name);
