08e07bcd653c9b1404869ae61d86f564efaeba8a
Lucky Lukert <philip.lukert@gmail.com>
2015-06-08 22:44:43 +0200

    gespielte Spiele-Ansicht hinzugefügt

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/view/ControllerRanking.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/view/ControllerRanking.java
@@ -1,32 +1,26 @@
 package org.pixelgaffer.turnierserver.codr.view;
 
 
-import org.pixelgaffer.turnierserver.codr.CodrAi;
-import org.pixelgaffer.turnierserver.codr.MainApp;
-import org.pixelgaffer.turnierserver.codr.ParticipantResult;
-import org.pixelgaffer.turnierserver.codr.Version;
-import org.pixelgaffer.turnierserver.codr.utilities.Resources;
-
-import javafx.beans.property.ObjectProperty;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Callback;
+
+import org.pixelgaffer.turnierserver.codr.CodrAi;
+import org.pixelgaffer.turnierserver.codr.CodrGame;
+import org.pixelgaffer.turnierserver.codr.MainApp;
+import org.pixelgaffer.turnierserver.codr.Version;
+import org.pixelgaffer.turnierserver.codr.utilities.Resources;
 
 
 
@@ -41,6 +35,7 @@ public class ControllerRanking {
 	@FXML TextArea tbDescription;
 	@FXML TableView<CodrAi> tvAis;
 	@FXML TableView<Version> tvVersions;
+	@FXML TableView<CodrGame> tvGames;
 	@FXML ImageView imageView;
 	
 	MainApp mainApp;
@@ -59,7 +54,8 @@ public class ControllerRanking {
 			clickChangeAi(newValue);
 		});
 		
-		loadOnlineAis();
+		mainApp.loadOnlineAis();
+		tvAis.setItems(MainApp.onlineAis);
 		
 		TableColumn<CodrAi, Image> col0 = new TableColumn<CodrAi, Image>("Bild");
 		col0.setMaxWidth(60);
@@ -118,10 +114,12 @@ public class ControllerRanking {
 		tvAis.getColumns().add(col2);
 		tvAis.getColumns().add(col3);
 		
+		
+		
 		TableColumn<Version, String> colV0 = new TableColumn<>("Version");
 		TableColumn<Version, String> colV1 = new TableColumn<>("Kompiliert");
 		TableColumn<Version, String> colV2 = new TableColumn<>("Qualifiziert");
-		TableColumn<Version, String> colV3 = new TableColumn<>("Fertiggestellt");
+		TableColumn<Version, String> colV3 = new TableColumn<>("Freigegeben");
 		
 		colV0.setCellValueFactory(new Callback<CellDataFeatures<Version, String>, ObservableValue<String>>() {
 			public ObservableValue<String> call(CellDataFeatures<Version, String> p) {
@@ -162,14 +160,22 @@ public class ControllerRanking {
 		tvVersions.getColumns().add(colV1);
 		tvVersions.getColumns().add(colV2);
 		tvVersions.getColumns().add(colV3);
+		tvVersions.setFixedCellSize(25);
 		
-	}
-	
-	
-	public void loadOnlineAis() {
-		mainApp.loadOnlineAis();
-		tvAis.setItems(MainApp.onlineAis);
-		tvAis.getSelectionModel().selectFirst();
+		
+
+		TableColumn<CodrGame, String> colG0 = new TableColumn<>("Gegner");
+		TableColumn<CodrGame, String> colG1 = new TableColumn<>("zum Spiel");
+		TableColumn<CodrGame, String> colG2 = new TableColumn<>("Datum");
+		TableColumn<CodrGame, String> colG3 = new TableColumn<>("gespielte Zeit");
+		TableColumn<CodrGame, String> colG4 = new TableColumn<>("Gewonnen?");
+		
+		tvGames.getColumns().add(colG0);
+		tvGames.getColumns().add(colG1);
+		tvGames.getColumns().add(colG2);
+		tvGames.getColumns().add(colG3);
+		tvGames.getColumns().add(colG4);
+		
 	}
 	
 	
@@ -190,6 +196,27 @@ public class ControllerRanking {
 			imageView.imageProperty().unbind();
 			imageView.imageProperty().bind(ai.getPicture());
 			tvVersions.setItems(ai.versions);
+			if (ai.versions.size() != 0){
+				tvVersions.prefHeightProperty().bind(tvVersions.fixedCellSizeProperty().multiply(Bindings.size(tvVersions.getItems()).add(1.25)));
+				tvVersions.minHeightProperty().bind(tvVersions.prefHeightProperty());
+				tvVersions.maxHeightProperty().bind(tvVersions.prefHeightProperty());
+			}
+			else{
+				tvVersions.prefHeightProperty().set(60);
+				tvVersions.minHeightProperty().set(60);
+				tvVersions.maxHeightProperty().set(60);
+			}
+			tvGames.setItems(ai.onlineGames);
+			if (ai.onlineGames.size() != 0){
+				tvGames.prefHeightProperty().bind(tvGames.fixedCellSizeProperty().multiply(Bindings.size(tvGames.getItems()).add(1.25)));
+				tvGames.minHeightProperty().bind(tvGames.prefHeightProperty());
+				tvGames.maxHeightProperty().bind(tvGames.prefHeightProperty());
+			}
+			else{
+				tvGames.prefHeightProperty().set(60);
+				tvGames.minHeightProperty().set(60);
+				tvGames.maxHeightProperty().set(60);
+			}
 		} else {
 			lbName.setText("Null");
 			tbDescription.setText("Aktuell wird keine KI angezeigt");
@@ -199,13 +226,26 @@ public class ControllerRanking {
 			btChallenge.setDisable(true);
 			imageView.imageProperty().set(Resources.defaultPicture());
 			tvVersions.setItems(null);
+			tvVersions.prefHeightProperty().set(60);
+			tvVersions.minHeightProperty().set(60);
+			tvVersions.maxHeightProperty().set(60);
+			tvGames.setItems(null);
+			tvGames.prefHeightProperty().set(60);
+			tvGames.minHeightProperty().set(60);
+			tvGames.maxHeightProperty().set(60);
 		}
 	}
 	
 	
 	@FXML public void clickChallenge() {
+		mainApp.loadOnlineAis();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		tvAis.setItems(MainApp.onlineAis);
-		tvAis.getSelectionModel().selectFirst();
 	}
 	
 	
