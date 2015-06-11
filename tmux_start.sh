24740761864a9d6df8f31da6f960c4e07a95f848
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-06-11 11:41:45 +0000

    gunicorn tests und minesweeper zeugs

--- tmux_start.sh
+++ tmux_start.sh
@@ -11,6 +11,8 @@ tmux new-window -t turnierserver:3 -n 'Worker' './build/worker.sh "../turnierser
 tmux new-window -t turnierserver:4 -n 'Sandbox 1' 'sleep 1; ./build/sandbox.sh SandboxMachine/SandboxMachine.ini; read'
 tmux new-window -t turnierserver:5 -n 'Sandbox 2' 'sleep 2; ./build/sandbox.sh SandboxMachine/SandboxMachine.ini; read'
 tmux new-window -t turnierserver:6 -n 'Frontend' 'cd Frontend; python3 app.py run; read'
+# für gunicorn:
+#tmux new-window -t turnierserver:6 -n 'Frontend' 'cd Frontend; gunicorn -b 0.0.0.0:80 app:app --log-level INFO; read'
 
 tmux select-window -t turnierserver:1
 tmux -2 attach-session -t turnierserver
