cb5de9c47d722a1f6322d055b5d2994e73590e4a
Lucky Lukert <philip.lukert@gmail.com>
2015-06-18 18:28:07 +0200

    Kleine Verbesserungen

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/WebConnector.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/WebConnector.java
@@ -432,7 +432,6 @@ public class WebConnector {
 			ErrorLog.write("Die Spieltypen konnten nicht aus der Datei gelesen werden: " + e);
 			ErrorLog.write("Es werden nun alle Spieltypen geladen!");
 		}
-		System.out.println(json);
 		
 		List<String> gametypesInFile = new ArrayList<>();
 		List<String> gametypesFromFrontend = new ArrayList<>();
