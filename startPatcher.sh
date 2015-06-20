0f92cb67aefe0cf00cb652f0e970bb42282b7b00
Dominic <meiserdo@web.de>
2015-06-20 17:52:49 +0200

    startPatcher.sh fixed

--- startPatcher.sh
+++ startPatcher.sh
@@ -7,16 +7,19 @@ cd `dirname $0` || exit 1
 logfile=$PWD/startPatcher.log
 # das Verzeichnis, in dem der Patcher ist
 directory=Patcher
-# das Verzeichnis, von dem aus ich aus dem Verzeichnis des Patchers ins git-root komme
-dirback=..
 # die ausführbare Datei des Patchers
 execfile=patcher
+# das Build-Verzeichnis
+builddir=PatcherBuild
 
 # die konfiguration ausgeben
 echo "Using logfile $logfile"
 echo "[$(date)] Using directory = $directory"  >$logfile
 echo "[$(date)] Using dirback   = $dirback"   >>$logfile
 echo "[$(date)] Using execfile  = $execfile"  >>$logfile
+echo "[$(date)] Using builddir  = $builddir"  >>$logfile
+
+export "LD_LIBRARY_PATH=$PWD/$builddir:$LD_LIBRARY_PATH"
 
 while [ true ]
 do
@@ -24,8 +27,8 @@ do
 	
 	# bauen
 	echo "[$(date)] Building the patcher" >>$logfile
-	mkdir -p PatcherBuild || exit 1
-	cd PatcherBuild || exit 1
+	mkdir -p $builddir || exit 1
+	cd $builddir || exit 1
 	qmake -makefile -r "../$directory/Patcher.pro" &>>$logfile || exit 1
 	make clean &>/dev/null
 	make -j4 &>>$logfile || exit 1
@@ -34,7 +37,7 @@ do
 	# den Patcher starten
 	echo "[$(date)] Starting the Patcher" >>$logfile
 	echo "[$(date)] Starting the Patcher"
-	"PatcherBuild/$execfile" ${@} &>>$logfile || exit 1
+	"$builddir/$execfile" ${@} &>>$logfile || exit 1
 done
 
 echo "[$(date)] This should not be executed. There is no good reason to leave the while loop without exiting directly. Please contact an administrator." >>$logfile
