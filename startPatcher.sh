3766c8d14734603ed1c0ed361d16ba44cde2c561
Dominic <meiserdo@web.de>
2015-06-20 14:28:44 +0200

    startPatcher.sh angepasst

--- startPatcher.sh
+++ startPatcher.sh
@@ -9,41 +9,32 @@ logfile=$PWD/startPatcher.log
 directory=Patcher
 # das Verzeichnis, von dem aus ich aus dem Verzeichnis des Patchers ins git-root komme
 dirback=..
-# die main-Klasse des Patchers
-mainclass=org.pixelgaffer.turnierserver.patcher.Patcher
+# die ausführbare Datei des Patchers
+execfile=patcher
 
 # die konfiguration ausgeben
 echo "Using logfile $logfile"
 echo "[$(date)] Using directory = $directory"  >$logfile
 echo "[$(date)] Using dirback   = $dirback"   >>$logfile
-echo "[$(date)] Using mainclass = $mainclass" >>$logfile
+echo "[$(date)] Using execfile  = $execfile"  >>$logfile
 
 while [ true ]
 do
-	cwd=$PWD
 	echo >>$logfile
 	
-	# das Patcher-Verzeichnis betreten
-	cd $directory 2&>1 >>$logfile || exit 1
-	# aufräumen
-#	echo "[$(date)] Cleaning old package of the patcher" >>$logfile
-#	mvn clean &>>$logfile || exit 1
 	# bauen
 	echo "[$(date)] Building the patcher" >>$logfile
-	mvn package &>>$logfile || exit 1
-	# dependencies holen
-	echo "[$(date)] Getting dependencies" >>$logfile
-	mvn dependency:copy-dependencies &>>$logfile || exit 1
+	mkdir -p PatcherBuild || exit 1
+	cd PatcherBuild || exit 1
+	qmake -makefile -r "../$directory/Patcher.pro" &>>$logfile || exit 1
+	make clean &>/dev/null
+	make -j4 &>>$logfile || exit 1
+	cd .. || exit 1
 	
-	# ins git-root gehen
-	cd $dirback &>>$logfile || exit 1
 	# den Patcher starten
 	echo "[$(date)] Starting the Patcher" >>$logfile
 	echo "[$(date)] Starting the Patcher"
-	java -cp "$directory/target/*:$directory/target/dependency/*" $mainclass ${@} &>>$logfile || exit 1
-	
-	# zurück ins aufruf-verzeichnis gehen
-	cd $cwd &>>$logfile || exit 1
+	"PatcherBuild/$execfile" ${@} &>>$logfile || exit 1
 done
 
 echo "[$(date)] This should not be executed. There is no good reason to leave the while loop without exiting directly. Please contact an administrator." >>$logfile
