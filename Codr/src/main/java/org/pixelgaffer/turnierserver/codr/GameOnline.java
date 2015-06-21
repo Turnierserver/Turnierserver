b17207fe9d0837ababe2b72d2ba969f3f6275bf3
nicosio2 <nico.gruendel@web.de>
2015-06-21 19:47:59 +0200

    Spiele werden nun geladen und eigene AIs werden intelligenter geholt

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/GameOnline.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/GameOnline.java
@@ -1,15 +1,32 @@
 package org.pixelgaffer.turnierserver.codr;
 
 
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.pixelgaffer.turnierserver.codr.utilities.WebConnector;
 
 
 
 public class GameOnline extends GameBase {
 	
+	private List<AiOnline> ais;
+	private int id;
+	private String gameType;
+	private int gameTypeId;
 	
-	public GameOnline(JSONObject json) {
+	public GameOnline(JSONObject json, WebConnector connector) {
 		super(GameMode.onlineLoaded);
+		id = json.getInt("id");
+		JSONObject gametypeObject = json.getJSONObject("type");
+		gameType = gametypeObject.getString("name");
+		gameTypeId = gametypeObject.getInt("id");
+		JSONArray aiArray = json.getJSONArray("ais");
+		for(int i = 0; i < aiArray.length(); i++) {
+			JSONObject aiObject = aiArray.getJSONObject(i);
+			ais.add(new AiOnline(json, connector));
+		}
 	}
 	
 	
