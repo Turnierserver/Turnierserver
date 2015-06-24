c6dfbe3760a43a575d0467cf55f8134f6c9536a4
Dominic <meiserdo@web.de>
2015-06-24 21:24:12 +0200

    philips dreck revertet

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/CodeEditor.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/CodeEditor.java
@@ -1,11 +1,14 @@
 package org.pixelgaffer.turnierserver.codr;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,15 +32,16 @@ import org.pixelgaffer.turnierserver.codr.utilities.ErrorLog;
 import org.pixelgaffer.turnierserver.codr.utilities.Paths;
 import org.xml.sax.SAXException;
 
-
-
-public class CodeEditor {
-	public static void writeSyntax() throws ParserConfigurationException, SAXException {
+public class CodeEditor
+{
+	public static void writeSyntax () throws ParserConfigurationException, SAXException
+	{
 		File syntaxFolder = new File(Paths.syntaxFolder());
 		if (syntaxFolder.exists())
 			return;
 		
-		try {
+		try
+		{
 			syntaxFolder.mkdirs();
 			File tmp = File.createTempFile("syntax", ".zip");
 			tmp.deleteOnExit();
@@ -46,24 +50,46 @@ public class CodeEditor {
 			zipFile.extractAll(syntaxFolder.getAbsolutePath());
 			
 			p = new Properties();
-			for (String filename : syntaxFolder.list()) {
+			for (String filename : syntaxFolder.list())
+			{
 				SyntaxParser parser = new SyntaxParser(new File(syntaxFolder, filename), new EmptyStyle());
 				for (String extension : parser.getExtensions())
 					p.put(extension.trim(), filename);
 			}
-			p.store(new FileOutputStream(new File(syntaxFolder, "index.prop")), "Enthält alle Syntax-Dateien sortiert nach den Dateienden");
-		} catch (ZipException | IOException e) {
+			p.store(new FileOutputStream(new File(syntaxFolder, "index.prop")),
+					"Enthält alle Syntax-Dateien sortiert nach den Dateienden");
+			
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(
+					new FileOutputStream(new File(syntaxFolder, "_fallback.xml")), UTF_8));
+			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			out.println("<!DOCTYPE language SYSTEM \"language.dtd\">");
+			out.println("<language name=\"fallback\" section=\"\" extensions=\"*\" mimetype=\"\" priority=\"-100\">");
+			out.println("  <highlighting>");
+			out.println("    <contexts>");
+			out.println("      <context name=\"cdata\" attribute=\"normal\" lineEndContext=\"#stay\">");
+			out.println("      </context>");
+			out.println("    </contexts>");
+			out.println("    <itemDatas>");
+			out.println("      <itemData name=\"normal\" defStyleNum=\"dsNormal\" />");
+			out.println("    </itemDatas>");
+			out.println("  </highlighting>");
+			out.println("</language>");
+			out.close();
+			
+		}
+		catch (ZipException | IOException e)
+		{
 			ErrorLog.write("Konnte Syntax nicht schreiben: " + e);
 			syntaxFolder.delete();
 		}
 	}
 	
-	
 	private static Properties p = null;
 	
-	
-	private static Properties props() throws IOException {
-		if (p == null) {
+	private static Properties props () throws IOException
+	{
+		if (p == null)
+		{
 			p = new Properties();
 			p.load(new FileInputStream(new File(Paths.syntaxFolder(), "index.prop")));
 		}
@@ -77,50 +103,78 @@ public class CodeEditor {
 	private CodeArea codeArea;
 	private SyntaxParser parser;
 	
-	
 	/**
 	 * Initialisiert den CodeEditor mit der zu zeigenden Datei
 	 * 
 	 * @param doc
 	 */
-	public CodeEditor(File doc) {
+	public CodeEditor (File doc)
+	{
 		document = doc;
 		codeArea = new CodeArea();
 		codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
 //		codeArea.setStyle("-fx-text-fill: white");
 		
 		Style style = new EmptyStyle();
-		try {
-			style = MainApp.cStart.btTheme.isSelected() ? Styles.getStyle("VibrantInk") : Styles.getStyle("KateDefault");
-		} catch (IOException e1) {
+		try
+		{
+			style = MainApp.cStart.btTheme.isSelected()
+					? Styles.getStyle("VibrantInk")
+					: Styles.getStyle("Eclipse");
+		}
+		catch (IOException e1)
+		{
 			e1.printStackTrace();
 		}
-		try {
-			for (Object o : props().keySet()) {
-				String extension = (String) o;
+		try
+		{
+			for (Object o : props().keySet())
+			{
+				String extension = (String)o;
 				extension = extension.replace(".", "\\.");
 				extension = extension.replace("*", ".*");
 				Pattern pattern = Pattern.compile(extension, Pattern.CASE_INSENSITIVE);
 				Matcher matcher = pattern.matcher(doc.getName());
-				if (matcher.matches()) {
-					parser = new SyntaxParser(new File(Paths.syntaxFolder(), (String) props().get(o)), style);
+				if (matcher.matches())
+				{
+					parser = new SyntaxParser(new File(Paths.syntaxFolder(), (String)props().get(o)), style);
 					break;
 				}
 			}
-		} catch (SAXException | ParserConfigurationException | IOException e) {
+		}
+		catch (SAXException | ParserConfigurationException | IOException e)
+		{
 			ErrorLog.write("Fehler beim Laden des SyntaxParser: " + e);
 			e.printStackTrace();
 		}
 		
-		if (parser != null) {
+		if (parser == null)
+		{
+			try
+			{
+				parser = new SyntaxParser(new File(Paths.syntaxFolder(), "_fallback.xml"), style);
+			}
+			catch (ParserConfigurationException | IOException | SAXException e)
+			{
+				ErrorLog.write("Fehler beim Laden des SyntaxParser: " + e);
+				e.printStackTrace();
+			}
+		}
+		
+		if (parser != null)
+		{
 			codeArea.setId("codeArea");
-			try {
+			try
+			{
 				codeArea.getStylesheets().add(parser.generateStylesheet("codeArea").toExternalForm());
-			} catch (IOException e) {
+			}
+			catch (IOException e)
+			{
 				ErrorLog.write("Fehler beim Laden eines generierten StyleSheets: " + e);
 				e.printStackTrace();
 			}
-			codeArea.textProperty().addListener((obs, oldText, newText) -> codeArea.setStyleSpans(0, parser.computeHighlighting(newText)));
+			codeArea.textProperty().addListener(
+					(obs, oldText, newText) -> codeArea.setStyleSpans(0, parser.computeHighlighting(newText)));
 		}
 		
 		load();
@@ -133,7 +187,8 @@ public class CodeEditor {
 	 * 
 	 * @return true, wenn savedText != text
 	 */
-	public boolean hasChanged() {
+	public boolean hasChanged ()
+	{
 		if (loaded)
 			return !savedText.equals(getCode());
 		else
@@ -141,12 +196,14 @@ public class CodeEditor {
 	}
 	
 	
-	public String getCode() {
+	public String getCode ()
+	{
 		return codeArea.getText();
 	}
 	
 	
-	public void setCode(String t) {
+	public void setCode (String t)
+	{
 		codeArea.replaceText(0, codeArea.getText().length() - 1, t);
 	}
 	
@@ -156,7 +213,8 @@ public class CodeEditor {
 	 * 
 	 * @return das erstellte Tab
 	 */
-	public Tab getView() {
+	public Tab getView ()
+	{
 		BorderPane pane = new BorderPane(codeArea);
 		return new Tab(document.getName(), pane);
 	}
@@ -165,13 +223,17 @@ public class CodeEditor {
 	/**
 	 * Lädt den Inhalt der Datei in die StringProperty text
 	 */
-	public void load() {
-		try {
+	public void load ()
+	{
+		try
+		{
 			setCode(FileUtils.readFileToString(document));
 			
 			savedText = getCode();
 			loaded = true;
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			ErrorLog.write("Quellcode konnte nicht gelesen werden: " + e);
 		}
 	}
@@ -180,7 +242,8 @@ public class CodeEditor {
 	/**
 	 * Speichert den Inhalt der StringProperty text in die Datei
 	 */
-	public void save() {
+	public void save ()
+	{
 		if (!hasChanged())
 			return;
 		forceSave();
@@ -191,13 +254,17 @@ public class CodeEditor {
 	 * Speichert den Inhalt der StringProperty text in eine Datei. Dabei wird
 	 * nicht Überprüft, ob sich der Inhalt verändert hat.
 	 */
-	public void forceSave() {
-		try {
+	public void forceSave ()
+	{
+		try
+		{
 			FileWriter writer = new FileWriter(document, false);
 			writer.write(getCode());
 			writer.close();
 			savedText = getCode();
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			ErrorLog.write("Quellcode konnte nicht bearbeitet werden");
 		}
 	}
