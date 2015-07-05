7602cc61676450f07a61ee7eb17865600bd9a654
Dominic <meiserdo@web.de>
2015-07-05 19:00:20 +0200

    zeugs für nico

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/Libraries.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/Libraries.java
@@ -1,22 +1,28 @@
 package org.pixelgaffer.turnierserver.codr.utilities;
 
-
 import java.io.File;
 
 import org.pixelgaffer.turnierserver.codr.MainApp;
 import org.pixelgaffer.turnierserver.compile.LibraryDownloader;
 
-
-
-public class Libraries implements LibraryDownloader {
-	
-	@Override public File[] getAiLibs(String language) {
+public class Libraries implements LibraryDownloader
+{
+	@Override
+	public File[] getAiLibs (String language)
+	{
 		return new File(Paths.ailibrary(MainApp.actualGameType.get(), language)).listFiles();
 	}
 	
-	
-	@Override public File[] getLib(String language, String name) {
-		throw new UnsupportedOperationException();
+	@Override
+	public File[] getLib (String language, String name)
+	{
+		File dir = new File(Paths.library(language, name));
+		if (dir.exists())
+			return dir.listFiles();
+		if (!dir.mkdirs())
+			throw new IllegalStateException("Can't download Library " + language + "/" + name + " to " + dir.getPath());
+		if (!MainApp.webConnector.getLibrary(language, name))
+			throw new IllegalStateException("Failed to download " + language + "/" + name);
+		return dir.listFiles();
 	}
-	
 }
--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/Paths.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/utilities/Paths.java
@@ -1,6 +1,5 @@
 package org.pixelgaffer.turnierserver.codr.utilities;
 
-
 import java.io.File;
 
 import org.pixelgaffer.turnierserver.codr.AiBase;
@@ -11,69 +10,93 @@ import org.pixelgaffer.turnierserver.codr.MainApp;
 import org.pixelgaffer.turnierserver.codr.ParticipantResult;
 import org.pixelgaffer.turnierserver.codr.Version;
 
-
-
-public class Paths {
+public class Paths
+{
 	
-	public static String syntaxFolder() {
+	public static String syntaxFolder ()
+	{
 		return "Syntax";
 	}
 	
 	
-	public static String newCodrVersion() {
+	public static String newCodrVersion ()
+	{
 		return "CodrNewVersion.jar";
 	}
 	
 	
-	public static String settings() {
+	public static String settings ()
+	{
 		return "settings.txt";
 	}
 	
 	
-	public static String downloadFolder() {
+	public static String downloadFolder ()
+	{
 		return "Downloads";
 	}
 	
 	
-	public static String sessionFile() {
+	public static String sessionFile ()
+	{
 		return "session.conf";
 	}
 	
 	
-	public static String langsFile() {
+	public static String langsFile ()
+	{
 		return downloadFolder() + "/langs.prop";
 	}
 	
 	
-	public static String gameTypesFile() {
+	public static String gameTypesFile ()
+	{
 		return downloadFolder() + "/gametypes.prop";
 	}
 	
 	
-	public static String downloadGameType(String game) {
+	public static String downloadGameType (String game)
+	{
 		return downloadFolder() + "/" + game;
 	}
 	
 	
-	public static String gameLogic(String game) {
+	public static String gameLogic (String game)
+	{
 		return downloadGameType(game) + "/gamelogic.jar";
+	}
+	
+	public static String downloadLibraries ()
+	{
+		return downloadFolder() + "/Libraries";
+	}
+	
+	/**
+	 * gibt den ordner der bibliothek zurück. <code>name</code> ist name/version.
+	 */
+	public static String library (String language, String name)
+	{
+		return downloadLibraries() + "/" + language + "/" + name;
 	}
 	
 	
 	/**
 	 * Gibt den Pfad zum SimplePlayer-Ordner zurück
 	 */
-	public static String simplePlayerFolder(String game) {
+	public static String simplePlayerFolder (String game)
+	{
 		return downloadGameType(game) + "/SimplePlayer";
 	}
 	
 	
-	public static String simplePlayer(String game, String language) {
+	public static String simplePlayer (String game, String language)
+	{
 		return downloadGameType(game) + "/SimplePlayer/SimplePlayer" + language + "/v0";
 	}
 	
 	
-	public static String ailibrary(String game, String language) {
+	public static String ailibrary (String game, String language)
+	{
 		return downloadGameType(game) + "/ailib/" + language;
 	}
 	
@@ -81,12 +104,14 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Spieler-Ordner zurück
 	 */
-	public static String aiFolder() {
+	public static String aiFolder ()
+	{
 		return "AIs/Intern";
 	}
 	
 	
-	public static String aiExternFolder() {
+	public static String aiExternFolder ()
+	{
 		return "AIs/Extern";
 	}
 	
@@ -94,7 +119,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Spiele-Ordner zurück
 	 */
-	public static String gameFolder() {
+	public static String gameFolder ()
+	{
 		return "Games";
 	}
 	
@@ -102,7 +128,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Ordner eines bestimmten Spiels zurück
 	 */
-	public static String game(GameBase game) {
+	public static String game (GameBase game)
+	{
 		return gameFolder() + "/" + game.ID;
 	}
 	
@@ -110,7 +137,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Ordner eines bestimmten Spiels zurück
 	 */
-	public static String game(String id) {
+	public static String game (String id)
+	{
 		return gameFolder() + "/" + id;
 	}
 	
@@ -118,7 +146,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zu den Properties eines bestimmten Spiels zurück
 	 */
-	public static String gameProperties(GameBase game) {
+	public static String gameProperties (GameBase game)
+	{
 		return game(game) + "/gameProperties.txt";
 	}
 	
@@ -126,12 +155,14 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zu dem Output der GameLogic für das Spiel zurück.
 	 */
-	public static String gameRenderData(GameBase game) {
+	public static String gameRenderData (GameBase game)
+	{
 		return game(game) + "/renderData.txt";
 	}
 	
 	
-	public static String participant(ParticipantResult part) {
+	public static String participant (ParticipantResult part)
+	{
 		return gameFolder() + "/" + part.game.ID + "/" + part.number + ".txt";
 	}
 	
@@ -139,12 +170,18 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Ordner eines bestimmten Spielers zurück
 	 */
-	public static String ai(AiBase ai) {
-		if (ai.mode == AiMode.simplePlayer) {
+	public static String ai (AiBase ai)
+	{
+		if (ai.mode == AiMode.simplePlayer)
+		{
 			return simplePlayerFolder(MainApp.actualGameType.get()) + "/" + ai.title;
-		} else if (ai.mode == AiMode.extern) {
+		}
+		else if (ai.mode == AiMode.extern)
+		{
 			return aiExternFolder() + "/" + ai.title;
-		} else {
+		}
+		else
+		{
 			return aiFolder() + "/" + ai.title;
 		}
 	}
@@ -153,7 +190,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zu den Properties eines Spielers zurück
 	 */
-	public static String aiProperties(AiBase ai) {
+	public static String aiProperties (AiBase ai)
+	{
 		return ai(ai) + "/aiProperties.txt";
 	}
 	
@@ -161,7 +199,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zum Bild eines Spielers zurück
 	 */
-	public static String aiPicture(AiBase ai) {
+	public static String aiPicture (AiBase ai)
+	{
 		if (new File(ai(ai) + "/picture.png").exists())
 			return ai(ai) + "/picture.png";
 		else if (new File(ai(ai) + "/picture.jpg").exists())
@@ -180,21 +219,22 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zu einer bestimmten Version zurück
 	 */
-	public static String version(Version version) {
+	public static String version (Version version)
+	{
 		if (version.ai.mode == AiMode.extern)
-			return ((AiExtern) version.ai).path;
+			return ((AiExtern)version.ai).path;
 		else
 			return ai(version.ai) + "/v" + version.number;
 	}
 	
 	
-	
 	/**
 	 * Gibt den Pfad zu einer bestimmten Version zurück
 	 */
-	public static String version(AiBase ai, int number) {
+	public static String version (AiBase ai, int number)
+	{
 		if (ai.mode == AiMode.extern)
-			return ((AiExtern) ai).path;
+			return ((AiExtern)ai).path;
 		else
 			return ai(ai) + "/v" + number;
 	}
@@ -203,7 +243,8 @@ public class Paths {
 	/**
 	 * Gibt den Pfad zu den Properties einer Version zurück
 	 */
-	public static String versionProperties(Version version) {
+	public static String versionProperties (Version version)
+	{
 		if (version.ai.mode == AiMode.extern)
 			return ai(version.ai) + "/versionProperties.txt";
 		else
@@ -211,17 +252,20 @@ public class Paths {
 	}
 	
 	
-	public static String versionSrc(Version version) {
+	public static String versionSrc (Version version)
+	{
 		return version(version) + "/src";
 	}
 	
 	
-	public static String versionSrcStartClass(Version version) {
+	public static String versionSrcStartClass (Version version)
+	{
 		return versionSrc(version) + "/settings.prop";
 	}
 	
 	
-	public static String versionBin(Version version) {
+	public static String versionBin (Version version)
+	{
 		if (version.ai.mode == AiMode.extern)
 			return ai(version.ai) + "/bin";
 		else
