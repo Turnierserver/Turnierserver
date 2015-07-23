8e7ab2891a69b0aae3dab8aaba495c4410ff14ee
nicosio2 <nico.gruendel@web.de>
2015-07-23 17:13:34 +0200

    Logger wird nun verwendet

--- Java Ai Library/src/main/java/org/pixelgaffer/turnierserver/ailibrary/Ai.java
+++ Java Ai Library/src/main/java/org/pixelgaffer/turnierserver/ailibrary/Ai.java
@@ -24,56 +24,53 @@ import com.google.gson.reflect.TypeToken;
  */
 public abstract class Ai<E, R> implements Runnable {
 
-	public static Logger logger = Logger.getLogger("Ai");
+    public static Logger logger = Logger.getLogger("Ai");
 
-	/**
-	 * Die Connection zum Worker
-	 */
-	private Socket con;
-	/**
-	 * Der Printwriter der Connection
-	 */
-	private PrintWriter out;
-	/**
-	 * Der BufferedReader der Connection
-	 */
-	private BufferedReader in;
+    /**
+     * Die Connection zum Worker
+     */
+    private Socket con;
+    /**
+     * Der Printwriter der Connection
+     */
+    private PrintWriter out;
+    /**
+     * Der BufferedReader der Connection
+     */
+    private BufferedReader in;
 
-	/**
-	 * Der kummulierte String von System.out
-	 */
-	protected StringBuilder output = new StringBuilder();
-	/**
-	 * Der momentane Gamestate des Servers
-	 */
-	protected Map<String, String> gamestate;
+    /**
+     * Der kummulierte String von System.out
+     */
+    protected StringBuilder output = new StringBuilder();
+    /**
+     * Der momentane Gamestate des Servers
+     */
+    protected Map<String, String> gamestate;
 
-	private TypeToken<R> token;
+    private TypeToken<R> token;
 
-	public Ai(TypeToken<R> token, String[] args) {
-		this.token = token;
-		try {
-			PropertyUtils.loadProperties(args.length > 0 ? args[0] : "ai.prop");
-			System.out.println("Connecting to " + PropertyUtils.getStringRequired(PropertyUtils.WORKER_HOST) + ":" + PropertyUtils.getIntRequired(PropertyUtils.WORKER_SERVER_PORT));
-			con = new Socket(PropertyUtils.getStringRequired(PropertyUtils.WORKER_HOST), PropertyUtils.getIntRequired(PropertyUtils.WORKER_SERVER_PORT));
-			out = new PrintWriter(con.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			System.out.println(PropertyUtils.getStringRequired(PropertyUtils.WORKER_SERVER_AICHAR));
-			System.out.println(PropertyUtils.getStringRequired(PropertyUtils.AI_UUID));
-			out.println(PropertyUtils.getStringRequired(PropertyUtils.WORKER_SERVER_AICHAR) + PropertyUtils.getStringRequired(PropertyUtils.AI_UUID));
-			System.out.println("Output stream wird nun umgeleitet (RIP)");
-			System.setOut(new PrintStream(new OutputStream() {
-				public void write(int b) throws IOException {
-					output.append((char) b);
-				}
-			}));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
+    public Ai(TypeToken<R> token, String[] args) {
+	this.token = token;
+	try {
+	    PropertyUtils.loadProperties(args.length > 0 ? args[0] : "ai.prop");
+	    logger.fine("Connecting to " + PropertyUtils.getStringRequired(PropertyUtils.WORKER_HOST) + ":" + PropertyUtils.getIntRequired(PropertyUtils.WORKER_SERVER_PORT));
+	    con = new Socket(PropertyUtils.getStringRequired(PropertyUtils.WORKER_HOST), PropertyUtils.getIntRequired(PropertyUtils.WORKER_SERVER_PORT));
+	    out = new PrintWriter(con.getOutputStream(), true);
+	    in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	    out.println(PropertyUtils.getStringRequired(PropertyUtils.WORKER_SERVER_AICHAR) + PropertyUtils.getStringRequired(PropertyUtils.AI_UUID));
+	    System.setOut(new PrintStream(new OutputStream() {
+		public void write(int b) throws IOException {
+		    output.append((char) b);
 		}
+	    }));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    System.exit(1);
 	}
+    }
 
-	/**
+    /**
 	 * Wird aufgerufen, sobald der Server ein Gamestate-Update sendet
 	 * 
 	 * @param state
@@ -84,84 +81,85 @@ public abstract class Ai<E, R> implements Runnable {
 	 */
 	protected abstract Object update(E state);
 
-	/**
+    /**
 	 * Gibt den momentanen Spielzustand zurück
 	 * 
 	 * @return Der momentane Spielzustand
 	 */
 	protected abstract E getState(R change);
 
-	public final void run() {
+    public final void run() {
 
-		try {
-			while (true) {
-				if (con.isClosed()) {
-					System.exit(0);
-				}
-				String line = in.readLine();
-				if (line == null)
-					throw new EOFException();
-				logger.info("JSON erhalten: " + line);
-				R updates = Parsers.getWorker().parse(Parsers.escape(line.getBytes("UTF-8")), token.getType());
-				logger.info("Geparsed zu: " + updates);
-				Object response = update(getState(updates));
-				logger.info("Sender response:" + response);
-				if (response != null) {
-					send(response);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
+	try {
+	    while (true) {
+		if (con.isClosed()) {
+		    System.exit(0);
 		}
-	}
-
-	/**
-	 * Sendet ein Objekt. Wenn ein Objekt nicht geparsed werden kann, oder wenn
-	 * bei rundenbasierten Spielen mehrere Objekte pro Runde gesendet werden,
-	 * verliert die ki automatisch
-	 * 
-	 * @param o
-	 */
-	protected final void send(Object o) {
-		try {
-			Parsers.escape(Parsers.getWorker().parse(o), con.getOutputStream());
-			con.getOutputStream().write(0xa);
-		} catch (IOException e) {
-			e.printStackTrace();
+		String line = in.readLine();
+		if (line == null)
+		    throw new EOFException();
+		logger.info("JSON erhalten: " + line);
+		R updates = Parsers.getWorker().parse(Parsers.escape(line.getBytes("UTF-8")), token.getType());
+		logger.info("Geparsed zu: " + updates);
+		Object response = update(getState(updates));
+		logger.info("Sender response:" + response);
+		if (response != null) {
+		    send(response);
 		}
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.exit(1);
 	}
+    }
 
-	/**
-	 * ACHTUNG: Mit dieser Methode gibt die KI automatisch auf
-	 */
-	public final void surrender() {
-		out.println("SURRENDER");
-		System.exit(0);
+    /**
+     * Sendet ein Objekt. Wenn ein Objekt nicht geparsed werden kann, oder wenn
+     * bei rundenbasierten Spielen mehrere Objekte pro Runde gesendet werden,
+     * verliert die ki automatisch
+     * 
+     * @param o
+     */
+    protected final void send(Object o) {
+	try {
+	    Parsers.escape(Parsers.getWorker().parse(o), con.getOutputStream());
+	    con.getOutputStream().write(0xa);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	/**
-	 * Muss in der Main-Methode aufgerufen werden, damit die KI sich zum Worker
-	 * verbinden kann
-	 */
-	public final void start() {
-		new Thread(this).start();
-		synchronized (this) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				surrender();
-			}
-		}
+    /**
+     * ACHTUNG: Mit dieser Methode gibt die KI automatisch auf
+     */
+    public final void surrender() {
+	out.println("SURRENDER");
+	System.exit(0);
+    }
+
+    /**
+     * Muss in der Main-Methode aufgerufen werden, damit die KI sich zum Worker
+     * verbinden kann
+     */
+    public final void start() {
+	new Thread(this).start();
+	synchronized (this) {
+	    try {
+		wait();
+	    } catch (InterruptedException e) {
+		surrender();
+	    }
 	}
-	
-	/**
-	 * Tolle Extension-Methode für xtend
-	 * 
-	 * @param builder Der Builder, der geleert werden soll
-	 */
-	protected void clear(StringBuilder builder) {
-		builder.delete(0, builder.length());
-	}
+    }
+
+    /**
+     * Tolle Extension-Methode für xtend
+     * 
+     * @param builder
+     *            Der Builder, der geleert werden soll
+     */
+    protected void clear(StringBuilder builder) {
+	builder.delete(0, builder.length());
+    }
 
 }
