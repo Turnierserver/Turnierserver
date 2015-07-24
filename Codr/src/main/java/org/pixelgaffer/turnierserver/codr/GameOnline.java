3166f1861ed91d291f311e3282bb5d7f99e1855d
nicosio2 <nico.gruendel@web.de>
2015-07-24 23:38:40 +0200

    OnlineAis laden jetz richtig

--- Codr/src/main/java/org/pixelgaffer/turnierserver/codr/GameOnline.java
+++ Codr/src/main/java/org/pixelgaffer/turnierserver/codr/GameOnline.java
@@ -1,31 +1,35 @@
 package org.pixelgaffer.turnierserver.codr;
 
 
-import java.util.List;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.pixelgaffer.turnierserver.codr.utilities.WebConnector;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 
 
 public class GameOnline extends GameBase {
 	
-	private List<AiOnline> ais;
+	private ObservableList<AiOnline> ais = FXCollections.observableArrayList();
 	private int id;
 	private String gameType;
 	private int gameTypeId;
 	
 	public GameOnline(JSONObject json, WebConnector connector) {
 		super(GameMode.onlineLoaded);
+		
 		id = json.getInt("id");
+		super.ID = id + "";
 		JSONObject gametypeObject = json.getJSONObject("type");
 		gameType = gametypeObject.getString("name");
 		gameTypeId = gametypeObject.getInt("id");
 		JSONArray aiArray = json.getJSONArray("ais");
-		for(int i = 0; i < aiArray.length(); i++) {
+		for (int i = 0; i < aiArray.length(); i++) {
 			JSONObject aiObject = aiArray.getJSONObject(i);
-			ais.add(new AiOnline(json, connector, false));
+			AiOnline ai = new AiOnline(aiObject, connector, false);
+			ais.add(ai);
 		}
 	}
 	
