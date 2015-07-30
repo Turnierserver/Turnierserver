4fa34f84c60873aad546b0510e599536bf328508
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-07-30 15:16:02 +0200

    Spielinfos als Tabelle

--- Frontend/backend.py
+++ Frontend/backend.py
@@ -184,7 +184,7 @@ class Backend(threading.Thread):
 					g = Game.from_inprogress(self.requests[reqid])
 					logger.debug(g)
 					self.requests[reqid]["finished_game_obj"] = g
-					pprint(self.requests[reqid])
+					#pprint(self.requests[reqid])
 
 			if "data" in d:
 				self.requests[reqid]["states"].append(d["data"])
