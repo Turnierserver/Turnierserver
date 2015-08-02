2cef4793227a940ab5290196d1dfbec895034132
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-08-02 12:21:23 +0200

    Spaß mit CORS

--- Frontend/views/anonymous.py
+++ Frontend/views/anonymous.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, abort, flash, url_for, request, Markup, send_file
+from flask import Blueprint, render_template, abort, flash, url_for, request, Markup, send_file, make_response, current_app
 from flask.ext.login import current_user
-import markdown
+from datetime import timedelta
+from functools import update_wrapper
 from database import AI, User, Game, GameType, Game_inprogress
 from backend import backend
 from logger import logger
 import json
+import markdown
 
 anonymous_blueprint = Blueprint("anonymous", __name__)
 
@@ -72,6 +74,49 @@ def inprogress_game(id):
 	stream = url_for("api.game_inprogress_log", id=game.id)
 	return render_template(game.type.viz, game=game, inprogress=True, ai0=game.ais[0], ai1=game.ais[1], stream=stream)
 
+
+def crossdomain(origin=None, methods=None, headers=None,
+				max_age=21600, attach_to_all=True,
+				automatic_options=True):
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, (str, bytes)):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, (str, bytes)):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
+
+	def get_methods():
+		if methods is not None:
+			return methods
+
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
+
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
+
+			h = resp.headers
+
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
+
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
+
 @anonymous_blueprint.route("/api.yaml")
+@crossdomain(origin='*', headers=["Content-Type"])
 def api_yaml():
 	return send_file("api.yaml")
