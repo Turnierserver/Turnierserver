2ce3afda39259fd897b559e5d732cc94b0a52d86
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-08-17 20:13:22 +0200

    --|=======>

--- Frontend/database.py
+++ Frontend/database.py
@@ -635,8 +635,8 @@ class Game(db.Model):
 
 		ai1gewonnen = 1 - ai0gewonnen
 
-		ai0eloneu = ai1elo + 32 * (ai0gewonnen - 1 / (1 + 10 ^ ((ai1.elo - ai0.elo) / 400)))
-		ai1eloneu = ai2elo + 32 * (ai1gewonnen - 1 / (1 + 10 ^ ((ai0.elo - ai1.elo) / 400)))
+		ai0eloneu = ai1elo + 32 * (ai0gewonnen - 1 / (1 + 10 ** ((ai1.elo - ai0.elo) / 400)))
+		ai1eloneu = ai2elo + 32 * (ai1gewonnen - 1 / (1 + 10 ** ((ai0.elo - ai1.elo) / 400)))
 		ai0.elo = ai0eloneu
 		ai1.elo = ai1eloneu
 		db.session.commit()
