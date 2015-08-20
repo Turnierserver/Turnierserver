85bb19961290d72458bd36041469dbb6bb68b16a
Dominic <meiserdo@web.de>
2015-08-20 13:16:25 +0200

    compiler creates no longer the start.sh file

--- CompilerLib/src/main/java/org/pixelgaffer/turnierserver/compile/PythonCompiler.java
+++ CompilerLib/src/main/java/org/pixelgaffer/turnierserver/compile/PythonCompiler.java
@@ -1,74 +1,65 @@
 package org.pixelgaffer.turnierserver.compile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.pixelgaffer.turnierserver.networking.DatastoreFtpClient;
 import it.sauronsoftware.ftp4j.FTPAbortedException;
 import it.sauronsoftware.ftp4j.FTPDataTransferException;
 import it.sauronsoftware.ftp4j.FTPException;
 import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
 import it.sauronsoftware.ftp4j.FTPListParseException;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Properties;
-
-import org.apache.commons.io.FileUtils;
-import org.pixelgaffer.turnierserver.networking.DatastoreFtpClient;
-
 public class PythonCompiler extends Compiler
-{	
+{
 	public PythonCompiler (int ai, int version, int game)
 	{
 		super(ai, version, game);
 	}
 	
+	public String getLanguage ()
+	{
+		return "Python";
+	}
+	
 	@Override
-	public boolean compile (File srcdir, File bindir, Properties p, PrintWriter output, LibraryDownloader libraryDownloader) throws IOException
+	public boolean compile (File srcdir, File bindir, Properties p, PrintWriter output, LibraryDownloader libraryDownloader)
+			throws IOException
 	{
 		// den wrapper laden
-		if (libraryDownloader == null) {
-			try {
+		if (libraryDownloader == null)
+		{
+			try
+			{
 				output.print("> Lade game_wrapper.py herunter ... ");
 				DatastoreFtpClient.retrieveAiLibrary(getGame(), "Python", bindir);
 				output.println("fertig");
 				output.print("> Lade wrapper.py herunter ... ");
 				DatastoreFtpClient.retrieveLibrary("wrapper", "Python", bindir);
 				output.println("fertig");
-			} catch (IOException | FTPIllegalReplyException | FTPException | FTPDataTransferException
-					| FTPAbortedException | FTPListParseException ioe) {
+			}
+			catch (IOException | FTPIllegalReplyException | FTPException | FTPDataTransferException
+					| FTPAbortedException | FTPListParseException ioe)
+			{
 				output.println(ioe.getMessage());
 				return false;
 			}
-		} else {
+		}
+		else
+		{
 			output.println("> Keine Verbindung zum FTP-Server");
 			libraryDownloader.getAiLibFile("Python", "game_wrapper.py");
 			libraryDownloader.getFile("Python", "wrapper", "wrapper.py");
 		}
 		
-
 		output.print("> Kopiere Quelltext ... ");
 		FileUtils.copyDirectory(srcdir, bindir);
 		output.println("fertig");
 		
-		// das script zum starten erzeugen
-		output.print("> Erstelle ein Skript zum Starten der KI ... ");
-		File scriptFile = new File(bindir, "start.sh");
-		scriptFile.createNewFile();
-		scriptFile.setExecutable(true);
-		if (!scriptFile.canExecute())
-		{
-			output.println("Konnte das Skript nicht als ausführbar markieren!");
-			return false;
-		}
-		PrintWriter script = new PrintWriter(new FileOutputStream(scriptFile));
-		script.println("#!/bin/sh");
-		// ## wenn libs pypy: pypy, p2k: python2
-		script.println("pythonExecutable=python3");
-		script.println("if [ -x \"$2\" ]; then\n\tpythonExecutable=\"$2\"\nfi");
-		script.println("\"$pythonExecutable\" wrapper.py \"" + p.getProperty("filename") + "\" \"$1\"");
-		script.close();
-		setCommand(new String[] { "python3", "wrapper.py", p.getProperty("filename") });
-		output.println("fertig");
+		setCommand("python3");
+		setArguments(new String[] { "wrapper.py", p.getProperty("filename") });
 		
 		return true;
 	}
