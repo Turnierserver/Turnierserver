ca66945a3bba7c8b124189c2e0047fe2cf04555a
Dominic <meiserdo@web.de>
2015-11-07 22:25:39 +0100

    Revert "4 sandboxen"
    This reverts commit 4e5e7f20fb0c5173d9c070364c629055f9b21e2e.

--- tmux_start.sh
+++ tmux_start.sh
@@ -10,11 +10,9 @@ tmux new-window -t turnierserver:2 -n 'Backend' './build/backend.sh "../turniers
 tmux new-window -t turnierserver:3 -n 'Worker' './build/worker.sh "../turnierserver.prop"; read'
 tmux new-window -t turnierserver:4 -n 'Sandbox 1' './build/sandbox.sh ../SandboxManager/sandbox.prop; read'
 tmux new-window -t turnierserver:5 -n 'Sandbox 2' './build/sandbox.sh ../SandboxManager/sandbox.prop; read'
-tmux new-window -t turnierserver:6 -n 'Sandbox 3' './build/sandbox.sh ../SandboxManager/sandbox.prop; read'
-tmux new-window -t turnierserver:7 -n 'Sandbox 4' './build/sandbox.sh ../SandboxManager/sandbox.prop; read'
-tmux new-window -t turnierserver:8 -n 'Frontend' 'cd Frontend; python3 app.py run; read'
+tmux new-window -t turnierserver:6 -n 'Frontend' 'cd Frontend; python3 app.py run; read'
 # für gunicorn:
-#tmux new-window -t turnierserver:8 -n 'Frontend' 'cd Frontend; gunicorn -b 0.0.0.0:80 app:app --log-level INFO; read'
+#tmux new-window -t turnierserver:6 -n 'Frontend' 'cd Frontend; gunicorn -b 0.0.0.0:80 app:app --log-level INFO; read'
 
 tmux select-window -t turnierserver:1
 tmux -2 attach-session -t turnierserver
