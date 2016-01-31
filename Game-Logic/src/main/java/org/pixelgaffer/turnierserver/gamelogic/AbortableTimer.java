2c52e30798f082e8467cd1fc1f6173293d04a406
Dominic <meiserdo@web.de>
2016-01-31 18:12:57 +0100

    fix nicos timer

--- Game-Logic/src/main/java/org/pixelgaffer/turnierserver/gamelogic/AbortableTimer.java
+++ Game-Logic/src/main/java/org/pixelgaffer/turnierserver/gamelogic/AbortableTimer.java
@@ -1,46 +1,66 @@
 package org.pixelgaffer.turnierserver.gamelogic;
 
-public class AbortableTimer {
-	
+public class AbortableTimer
+{
 	private boolean aborted = false;
 	private Thread timerThread;
 	private int millis;
+	private long start;
 	private Object lock = new Object();
 	private Runnable run;
 	
-	public AbortableTimer(int millis, Runnable run) {
+	public AbortableTimer (int millis, Runnable run)
+	{
 		this.millis = millis;
 		this.run = run;
 	}
 	
-	private void timer() {
-		synchronized (lock) {
-			try {
-				lock.wait(millis);
-			} catch (InterruptedException e) {
+	private void timer ()
+	{
+		synchronized (lock)
+		{
+			try
+			{
+				while (start + millis > System.currentTimeMillis())
+				{
+					long wait = millis - System.currentTimeMillis() + start;
+					System.out.println("warte " + wait);
+					lock.wait(wait);
+					System.out.println("habe " + wait + " gewartet");
+				}
+				System.out.println("bin fertig mit warten");
+			}
+			catch (InterruptedException e)
+			{
 				e.printStackTrace();
 				aborted = true;
 			}
 		}
-		if(!aborted) {
+		if (!aborted)
 			run.run();
-		}
+		else
+			System.out.println("wurde geabortet");
 		timerThread = null;
 	}
 	
-	public void abort() {
+	public void abort ()
+	{
 		aborted = true;
-		synchronized (lock) {
+		synchronized (lock)
+		{
 			lock.notifyAll();
 		}
 	}
 	
-	public void restart() {
-		if(timerThread != null) {
-			abort();
+	public void restart ()
+	{
+		start = System.currentTimeMillis();
+		aborted = false;
+		if (timerThread == null)
+		{
+			timerThread = new Thread(this::timer);
+			timerThread.start();
 		}
-		timerThread = new Thread(this::timer);
-		timerThread.start();
 	}
 	
 }
